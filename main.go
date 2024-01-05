c9b972e32ebb4af998235f25a173c65439b16b42
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2024-01-05 13:29:40 +0100

    deploy app on Kubernetes env

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+		return c.HTML(http.StatusOK, "Hello, If you are seeing this, it means the microservice project worked!")
 	})
 
 	e.GET("/health", func(c echo.Context) error {
